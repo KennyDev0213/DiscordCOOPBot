39e494effa0873596b098e49a85891993e70a560
KennyDev0213 <58504944+KennyDev0213@users.noreply.github.com>
2021-10-29 23:26:50 -0700

    init

---
+++ commands.py
@@ -0,0 +1,110 @@
+import random
+
+#-----init-----
+players = []
+
+#------program utility (for the bot/scripts)------
+#return true if player is found, false if not
+def checkPlayers(player_name) -> bool:
+  for player in players:
+    if player == player_name:
+      return True
+  return False
+
+#-----user utility (for commands)-----
+def shuffle(team_num=2):
+  
+  #if the length of the player array is 0 then just return a prompt
+  if len(players) == 0:
+    return "roster is empty add players with '!aoe add [player]'"
+
+  #initialize the teams list
+  teams = []
+  for _ in range(team_num):
+    teams.append([])
+
+  #copy the players list
+  player_copy = players.copy()
+  pointer = 0
+
+  while len(player_copy) != 0:
+    
+    #select a random player and set it as a queued player
+    queued_player = random.choice(player_copy)
+    
+    #removed the queued player from the list
+    player_copy.remove(queued_player)
+
+    #add the player to the team and tick the team pointer up
+    team = teams[pointer]
+    team.append(queued_player)
+
+    #if the team pointer is going over, reset it to 0
+    pointer += 1
+    if pointer >= team_num:
+      pointer = 0 
+
+  #build the string
+  team_str = '--------------------------------\n\n'
+  for team in teams:
+    for member in team:
+      team_str += member + "\n"
+    team_str += '\n--------------------------------\n\n'
+
+  return team_str
+
+#reset the playerlist to 0
+def resetPlayers():
+  try:
+    players.clear()
+    return "players have been reset!"
+  except:
+    return "there has been an issue reseting the players"
+
+#builds a help msg to prompt users
+def buildHelpMsg():
+  help_msg = ""
+  help_msg += "CO-OP gaming aoe team manager bot written by Kennon Dong\n\n"
+  help_msg += "use '!aoe' as the prefix\n"
+  help_msg += "type '!aoe shuffle [number of teams]' to generate teams\n"
+  help_msg += "type '!aoe add [player]' to add a player to the roster \n"
+  help_msg += "type '!aoe remove [player]' to remove a player from the roster\n"
+  help_msg += "type '!aoe all' to view all current player in the roster\n"
+  help_msg += "type '!aoe reset' to clear all current player in the roster\n"
+  return help_msg
+
+#-----player management-----
+
+#add player, return msg of status
+def addPlayer(player_name=""):
+  if player_name == "":
+    return "error: no player name found"
+  elif checkPlayers(player_name):
+    return player_name + " already in roster"
+  else:
+    players.append(player_name)
+    return player_name + " added to roster"
+
+#remove player, return msg of status
+def removePlayer(player_name=""):
+  if player_name == "":
+    return "error: no player name found"
+  elif checkPlayers(player_name):
+    players.remove(player_name)
+    return player_name + " was removed from roster"
+  else:
+    return player_name + " is not found in roster"
+
+#show all the players in current roster
+def showPlayers():
+  #if player list is empty return the empty status
+  if len(players) == 0:
+    return "roster is empty"
+  #else build the string
+  else:
+    all_players = ""
+    count = 1
+    for player in players:
+      all_players += str(count) +". " + str(player) + "\n"
+      count += 1
+    return all_players
---
+++ main.py
@@ -0,0 +1,81 @@
+import discord
+import os
+
+#command file that stores all the commands
+import commands as cmd
+
+#get the client for this computer
+client = discord.Client()
+
+#discord event
+@client.event
+async def on_ready():
+  print('loggin as {0.user}'.format(client))
+
+#discord event
+@client.event
+async def on_message(msg):
+
+  cmd_msg = ""
+
+  #if the message is from the bot or doesnt start with '!aoe' then ignore
+  if msg.author == client.user or not msg.content.startswith('!aoe'):
+    return
+  #else break down the msg
+  else:
+    cmd_msg = msg.content.split(" ")
+    try:
+      cmd_msg[1] = cmd_msg[1].lower()
+    except:
+      await msg.channel.send("invalid syntax \n\n" + cmd.buildHelpMsg())
+      return
+
+  #dev testing block
+  if cmd_msg[1] == "test":
+    broken_msg = msg.content.split(" ")
+    await msg.channel.send(broken_msg[2])
+
+  #shuffle the teams!
+  elif cmd_msg[1] == "shuffle":
+    team_num=2
+    try:
+      # try to parse the second command as an int, if int is empty or unparsable default it to '2'
+      try:
+        team_num = int(cmd_msg[2])
+        print(team_num)
+      except:
+        team_num = 2
+      await msg.channel.send(cmd.shuffle(team_num))
+    except Exception as e:
+      await msg.channel.send("uh oh something went wrong! -->" + str(e))
+    
+  #add a new player!
+  elif cmd_msg[1] == "add":
+    await msg.channel.send(cmd.addPlayer(cmd_msg[2]))
+
+  #remove a player!
+  elif cmd_msg[1] == "remove":
+    await msg.channel.send(cmd.removePlayer(cmd_msg[2]))
+
+  #show all players!
+  elif cmd_msg[1] == "all":
+    await msg.channel.send(cmd.showPlayers())
+
+  #reset the roster
+  elif cmd_msg[1] == "reset":
+    await msg.channel.send(cmd.resetPlayers())
+
+  #if the command was not recognized then send help msg
+  else:
+    await msg.channel.send(cmd.buildHelpMsg())
+
+#main function
+def main():
+  client.run(os.getenv('TOKEN'))
+  return
+
+#python practice
+if __name__ == "__main__":
+  main()
+
+  
